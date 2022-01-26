eebf5b3276ceb36c115a619d4ad73c54dfe4d5e6
lauvr <77846627+lauvr@users.noreply.github.com>
2022-01-26 07:22:22 -0500

    Add files via upload

---
+++ MyTest.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTest : MonoBehaviour
+{
+    MyVector2D firstVector = new MyVector2D(1, 3);
+    MyVector2D secondVector = new MyVector2D(2, -1);
+    MyVector2D suma;
+    MyVector2D resta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        suma = firstVector.add(secondVector);
+        resta = firstVector.substract(secondVector);
+
+        Debug.Log(suma);
+        Debug.Log(resta);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        firstVector.Draw();
+        secondVector.Draw();
+
+        suma.Draw();
+        resta.Draw();
+    }
+}
---
+++ MyVector2D.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyVector2D 
+{
+    public float x;
+    public float y;
+
+    public MyVector2D(float xComponent, float yComponent)
+    {
+        x = xComponent;
+        y = yComponent;
+    }
+
+    public void Draw()
+    {
+        Debug.DrawLine(new Vector2(0, 0), new Vector2(x, y));
+    }
+
+
+    public override string ToString()
+    {
+        return ("(" + x + ", " + y + ")");
+    }
+
+    public MyVector2D add(MyVector2D vector)
+    {
+        MyVector2D result = new MyVector2D(x + vector.x, y + vector.y);
+        return result;
+    }
+
+    public MyVector2D substract(MyVector2D vector)
+    {
+        MyVector2D result2 = new MyVector2D(x - vector.x, y - vector.y);
+        return result2;
+    }
+
+}
