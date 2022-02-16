82d433571939a6227cd991af573fa2b7c72c63fe
lauvr <77846627+lauvr@users.noreply.github.com>
2022-02-16 07:41:37 -0500

    Add files via upload

--- Mover.cs
+++ Mover.cs
@@ -1,22 +1,59 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MyMath;
 
-public class Mover : MonoBehaviour
-{
-    //[SerializeField]
-    Vector3 displacement;
-    [SerializeField]
-    Vector3 velocity;
+public class Mover : MonoBehaviour {
+	
+	[SerializeField]
+	Vector3 acceleration;
 
-    public void Update()
-    {
-        Move();
-    }
+	[SerializeField]
+	[Range(0,1)] float damping = 0.9f;
 
-    public void Move()
-    {
-        displacement = velocity * Time.deltaTime;
-        transform.position = transform.position + displacement;
-    }
+	Vector3 displacement;
+	Vector3 velocity;
+	[SerializeField]float xBorder, yBorder;
+
+	public void Update()
+	{
+		Move();
+		CheckCollision ();
+	}
+
+	public void Move()
+	{
+		velocity += acceleration * Time.deltaTime;
+
+		displacement = velocity * Time.deltaTime;
+
+		transform.position = transform.position + displacement;
+
+		velocity.Draw (transform.position, Color.green);
+		transform.position.Draw (Color.red);
+	}
+
+	public void CheckCollision(){
+
+		if (transform.position.x >= xBorder || transform.position.x <= -xBorder) {
+			velocity.x = velocity.x * -1 * damping;
+		}
+		if (transform.position.y >= yBorder || transform.position.y <= -yBorder) {
+			velocity.y = velocity.y * -1 * damping;
+		}
+
+
+		if (transform.position.x > xBorder ) {
+			transform.position = new Vector3(xBorder, transform.position.y) ;
+		}
+		if (transform.position.x < -xBorder) {
+			transform.position = new Vector3(-xBorder, transform.position.y);
+		}
+		if (transform.position.y > yBorder) {
+			transform.position = new Vector3(transform.position.x, yBorder);
+		}
+		if (transform.position.y < -yBorder) {
+			transform.position = new Vector3(transform.position.x, -yBorder);;
+		}
+	}
 }
